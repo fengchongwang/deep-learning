bbc3e5cc2d08fb6f51b1b0f29291b987544f0e4f
wang <git@github.com>
2018-02-09 21:52:27 +0100

    final version

--- exercises/tf_linear_regression.py
+++ exercises/tf_linear_regression.py
@@ -4,9 +4,9 @@ import numpy as np
 from pytz.tzinfo import _epoch
 tf.reset_default_graph()
 lr = 0.01
-x = np.arange(-1, 100, 0.2)
-y = x*2.3 + 6 + np.random.normal(scale = 0.2)
-batch_size = 64
+x = np.arange(-1, 100, 0.01)
+y = x*2.3 + 10 + np.random.normal(scale = 0.2)
+batch_size = 1024
 X = tf.placeholder(tf.float64, [batch_size,1])
 Y = tf.placeholder(tf.float64, [batch_size,1])
 W = tf.get_variable("weight", (1,1), tf.float64, initializer = tf.random_normal_initializer())
@@ -17,12 +17,12 @@ def forward_prop(x, w, b):
 def calculate_cost(y, x, w, b):
     return tf.losses.mean_squared_error(y, forward_prop(x, w, b))
 
-optimizer = tf.train.AdamOptimizer(learning_rate = 0.1).minimize(calculate_cost(Y, X, W, B))
+optimizer = tf.train.AdamOptimizer(learning_rate = .6).minimize(calculate_cost(Y, X, W, B))
 
     
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for _epoch in range(20):
+    for _epoch in range(50):
         shuffled_indices = list(range(len(x)))
         shuffle(shuffled_indices)
         begin_ind = 0
