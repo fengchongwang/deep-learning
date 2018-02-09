af5a73bf5c129e60e72c50bd646f03ff8e1adef1
wang <wang@laptop>
2018-02-09 15:54:47 +0100

    some improvements

--- exercises/tf_linear_regression.py
+++ exercises/tf_linear_regression.py
@@ -1,3 +1,4 @@
+from random import shuffle
 import tensorflow as tf
 import numpy as np
 from pytz.tzinfo import _epoch
@@ -5,11 +6,11 @@ tf.reset_default_graph()
 lr = 0.01
 x = np.arange(-1, 100, 0.2)
 y = x*2.3 + 6 + np.random.normal(scale = 0.2)
-X = tf.placeholder(float64, x.shape)
-Y = tf.placeholder(float64, y.shape)
 batch_size = 64
-W = tf.get_variable("weight", [1,1], float32)
-B = tf.get_variable("bias", [1,1], float32)
+X = tf.placeholder(tf.float32, [batch_size,1])
+Y = tf.placeholder(tf.float32, [batch_size,1])
+W = tf.get_variable("weight", [1,1],tf.float32 )
+B = tf.get_variable("bias", [1,1],tf.float32)
 def forward_prop(x, w, b):
     return tf.matmul(x, w) + b
 
@@ -17,15 +18,18 @@ def calculate_cost(y, x, w, b):
     return tf.losses.mean_squared_error(y, forward_prop(x, w, b))
     
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    sess.run(tf.initialize_all_variables())
     for _epoch in range(5):
-        shuffled_indices = np.random.shuffle(range(len(x)))
+        shuffled_indices = list(range(len(x)))
+        shuffle(shuffled_indices)
         begin_ind = 0
         end_ind = begin_ind + batch_size
-        while end_ind < len(shuffled_indices):
+        while begin_ind < len(shuffled_indices):
+            print(begin_ind)
+            sess.run(tf.train.AdamOptimizer(learning_rate = 0.1).minimize(calculate_cost(Y, X, W, B)),
+                     feed_dict = {X: x[begin_ind:end_ind,None], Y: y[begin_ind:end_ind,None]})
+            cost = sess.run(calculate_cost(y, x, w, b), 
+                     feed_dict = {X: x[begin_ind:end_ind,None], Y: y[begin_ind:end_ind,None]})
+            print(cost)
             begin_ind = end_ind
             end_ind = min(len(shuffled_indices), end_ind + batch_size) 
-
-            
-    cost = sess.run(X,Y,feed_dict = {X: })
-
